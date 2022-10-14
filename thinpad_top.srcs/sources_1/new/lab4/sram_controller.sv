f1c105a4695587adb734cf1b38290f667a5d9b96
BoxWorld <boxworld0088@gmail.com>
2022-10-14 22:09:55 +0800

    finish lab 4

--- thinpad_top.srcs/sources_1/new/lab4/sram_controller.sv
+++ thinpad_top.srcs/sources_1/new/lab4/sram_controller.sv
@@ -31,6 +31,96 @@ module sram_controller #(
     output reg [SRAM_BYTES-1:0] sram_be_n
 );
 
-  // TODO: 实现 SRAM 控制器
+  // SRAM 控制器
+  typedef enum logic [2:0] {
+    ST_IDLE = 0,
+    ST_READ = 1,
+    ST_READ_2 = 2,
+    ST_WRITE = 3,
+    ST_WRITE_2 = 4,
+    ST_WRITE_3 = 5,
+    ST_DONE = 6
+  } state_t;
+
+  state_t state, state_n;
+
+  // reset and state change
+  always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+      state <= ST_IDLE;
+    end else begin
+      state <= state_n;
+    end
+  end
+
+  // states
+  always_comb begin
+    state_n = state;
+    case (state)
+      ST_IDLE: begin
+        if (wb_stb_i && wb_cyc_i) begin
+          if (wb_we_i) begin
+            state_n = ST_WRITE;
+          end else begin
+            state_n = ST_READ;
+          end
+        end
+      end
+
+      ST_READ: begin
+        state_n = ST_READ_2;
+      end
+
+      ST_READ_2: begin
+        state_n = ST_DONE;
+      end
+
+      ST_WRITE: begin
+        state_n = ST_WRITE_2;
+      end
+
+      ST_WRITE_2: begin
+        state_n = ST_WRITE_3;
+      end
+
+      ST_WRITE_3: begin
+        state_n = ST_DONE;
+      end
+
+      ST_DONE: begin
+        state_n = ST_IDLE;
+      end
+
+      default: begin
+        state_n = ST_IDLE;
+      end
+    endcase
+  end
+
+  // signals
+  always_comb begin
+    sram_ce_n = (state == ST_IDLE) || (state == ST_DONE);
+    sram_oe_n = !((state == ST_READ) || (state == ST_READ_2));
+    sram_we_n = !(state == ST_WRITE_2);
+    wb_ack_o = (state == ST_DONE);
+    sram_addr = wb_adr_i[21: 2];
+    sram_be_n = (state == ST_WRITE || state == ST_WRITE_2 || state == ST_WRITE_3) ? ~wb_sel_i : '0;
+  end
+
+  // always_comb begin
+  //   case (state)
+  //     ST_WRITE, ST_WRITE_2, ST_WRITE_3: begin
+  //       sram_be_n = 4'b1111 - wb_sel_i;
+  //     end
+
+  //     default: begin
+  //       sram_be_n = '0;
+  //     end
+  //   endcase
+  // end
+
+  // data processing
+  assign sram_data = sram_we_n ? 32'bz : wb_dat_i;
+  assign wb_dat_o = sram_data;
 
 endmodule
--- thinpad_top.srcs/sources_1/new/lab5/lab5_master.sv
+++ thinpad_top.srcs/sources_1/new/lab5/lab5_master.sv
@@ -19,5 +19,74 @@ module lab5_master #(
 );
 
   // TODO: 实现实验 5 的内存+串口 Master
+  typedef enum logic [3:0] {
+    ST_IDLE = 0,
+    ST_READ_WAIT_ACTION = 1,
+    ST_READ_WAIT_CHECK = 2,
+    ST_READ_DATA_ACTION = 3,
+    ST_READ_DATA_DONE = 4,
+    ST_WRITE_SRAM_ACTION = 5,
+    ST_WRITE_SRAM_DONE = 6,
+    ST_WRITE_WAIT_ACTION = 7,
+    ST_WRITE_WAIT_CHECK = 8,
+    ST_WRITE_DATA_ACTION = 9,
+    ST_WRITE_DATA_DONE = 10
+  } state_t;
+
+  state_t state, state_n;
+
+  // reset and state change
+  always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+      state <= ST_IDLE;
+    end else begin
+      state <= state_n;
+    end
+  end
+
+  // states
+  // always_comb begin
+  //   state_n = state;
+  //   case (state)
+  //     ST_IDLE: begin
+  //       if (wb_stb_o && wb_cyc_o) begin
+  //         if (wb_we_i) begin
+  //           state_n = ST_WRITE;
+  //         end else begin
+  //           state_n = ST_READ;
+  //         end
+  //       end
+  //     end
+
+  //     ST_READ: begin
+  //       state_n = ST_READ_2;
+  //     end
+
+  //     ST_READ_2: begin
+  //       state_n = ST_DONE;
+  //     end
+
+  //     ST_WRITE: begin
+  //       state_n = ST_WRITE_2;
+  //     end
+
+  //     ST_WRITE_2: begin
+  //       state_n = ST_WRITE_3;
+  //     end
+
+  //     ST_WRITE_3: begin
+  //       state_n = ST_DONE;
+  //     end
+
+  //     ST_DONE: begin
+  //       state_n = ST_IDLE;
+  //     end
+
+  //     default: begin
+  //       state_n = ST_IDLE;
+  //     end
+  //   endcase
+  // end
 
 endmodule
+ 
